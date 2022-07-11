7887a50ebe0873539ce99dbb041427ba820439e1
elias <bianchi.elias@gmail.com>
2022-07-11 20:53:01 -0300

    Ajax CRUD finalizado
    Se agregó la funcion editar y eliminar que se encargan de mandar las peticiones al servidor para ejectuar su accion correspondiente junto con los datos necesarios.

--- controllers/ajax.controller.js
+++ controllers/ajax.controller.js
@@ -5,20 +5,20 @@ const d = document,
 	$template = d.getElementById('crud-template').content,
 	$fragment = d.createDocumentFragment();
 
-const getAllWhitAjax = () =>{
+const getAllWhitAjax = () => {
 	ajax({
 		url: "http://localhost:5555/santos",
 		success: (res) => {
 			// console.log(res)
-			res.forEach(el => {
-				$template.querySelector('.name').textContent = el.nombre
-				$template.querySelector('.constellation').textContent = el.constelacion
+			res.forEach(e => {
+				$template.querySelector('.name').textContent = e.nombre
+				$template.querySelector('.constellation').textContent = e.constelacion
 				
-				$template.querySelector('.edit').dataset.id = el.id
-				$template.querySelector('.edit').dataset.nombre = el.nombre
-				$template.querySelector('.edit').dataset.constelacion = el.constelacion
+				$template.querySelector('.edit').dataset.id = e.id
+				$template.querySelector('.edit').dataset.name = e.nombre
+				$template.querySelector('.edit').dataset.constellation = e.constelacion
 				
-				$template.querySelector('.delete').dataset.id = el.id
+				$template.querySelector('.delete').dataset.id = e.id
 				
 				let $clone = d.importNode($template, true)
 				$fragment.appendChild($clone)
@@ -27,11 +27,43 @@ const getAllWhitAjax = () =>{
 			$table.querySelector('tbody').appendChild($fragment)
 
 		},
-		error: (err) => {
-			// console.log(err)
-			$table.insertAdjacentHTML('afterend', `<p><b>${err}</b></p>`)
-		}
+		error: (err) => $table.insertAdjacentHTML('afterend', `<p><b>${err}</b></p>`)
 	})
 }
 
-export { getAllWhitAjax }
+const createWhitAjax = (data) => {
+	ajax({
+		url: "http://localhost:5555/santos",
+		method: "POST",
+		success: () => location.reload(),
+		error: () => $table.insertAdjacentHTML('afterend', `<p><b>${err}</b></p>`),
+		data
+	})
+}
+
+const editWhitAjax = (data) => {
+	const {id, ...nameAndConstellation} = data
+	ajax({
+		url: `http://localhost:5555/santos/${id}`,
+		method: "PUT",
+		success: () => location.reload(),
+		error: () => $table.insertAdjacentHTML('afterend', `<p><b>${err}</b></p>`),
+		data: nameAndConstellation
+	})
+}
+
+const deleteWhitAjax = (data) => {
+	ajax({
+		url: `http://localhost:5555/santos/${data}`,
+		method: "DELETE",
+		success: () => location.reload(),
+		error: () => $table.insertAdjacentHTML('afterend', `<p><b>${err}</b></p>`),
+	})
+}
+
+export { 
+	getAllWhitAjax,
+	createWhitAjax,
+	editWhitAjax,
+	deleteWhitAjax
+}
--- script.js
+++ script.js
@@ -1,12 +1,53 @@
-import { getAllWhitAjax } from "./controllers/ajax.controller.js";
+import {
+	getAllWhitAjax,
+	createWhitAjax,
+	editWhitAjax,
+	deleteWhitAjax
+} from "./controllers/ajax.controller.js";
 
 const d = document,
-	// $table = d.querySelector('.crud-table'),
 	$form = d.querySelector('.crud-form'),
 	$title = d.querySelector('.crud-title');
-	// $template = d.getElementById('crud-template').content,
-	// $fragment = d.createDocumentFragment();
-
-
 
 d.addEventListener("DOMContentLoaded", getAllWhitAjax)
+
+d.addEventListener("submit", e => {
+	if(e.target === $form){
+		e.preventDefault()
+
+		if(!e.target.id.value){
+			//CREATE - POST
+			createWhitAjax({
+				nombre: e.target.nombre.value,
+				constelacion: e.target.constelacion.value
+			})
+		}else{
+			//UPDATE - PUT
+			editWhitAjax({
+				id: e.target.id.value,
+				nombre: e.target.nombre.value,
+				constelacion: e.target.constelacion.value
+			})
+		}
+	}
+})
+
+d.addEventListener("click", e => {
+	if(e.target.matches(".edit")){
+		$title.textContent = "Editar Santo"
+		$form.nombre.value = e.target.dataset.name
+		$form.constelacion.value = e.target.dataset.constellation
+		$form.id.value = e.target.dataset.id
+	}
+})
+
+d.addEventListener("click", e => {
+	if(e.target.matches(".delete")){
+
+		let isDelete = confirm(`¿Estás seguro de eliminar el id ${e.target.dataset.id}?`)
+
+		if(isDelete){
+			deleteWhitAjax( e.target.dataset.id )
+		}
+	}
+})
